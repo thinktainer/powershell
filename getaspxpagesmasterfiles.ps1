b99037580fae40b130b99032b9a6c338c7439a55
Martin Schinz <m.schinz@gmail.com>
2012-07-03 18:02:28 +0100

    trying to come back from fail

--- getaspxpagesmasterfiles.ps1
+++ getaspxpagesmasterfiles.ps1
@@ -1,37 +1,17 @@
 $ErrorActionPreference = "Stop"
-Import-Module mem -Force
+$NO_MASTER_PAGE = 'none'
 
-Function Get-AspxPages([string] $solutionDir){
-	$webDirs = Get-WebDirectories($solutionDir)
-	Write-Debug("$webDirs")
-	$aspxPagePaths = foreach($webDir in $webDirs){
-		Get-AspxFilePaths($webDir)
-	}
-	Write-Debug("$aspxPagePaths")
-	$pagesWithMasterPage = foreach($path in $aspxPagePaths){
-		Find-MasterPagesInPages($path)
-	}
-	$pagesWithMasterPage
-}
+if(Get-Module Memasp) {Remove-Module Memasp}
+Import-Module Memasp 
 
-Function Get-UniqueMasterPages($objects){
-	$uniqueObjects = $objects | Group-Object -Property MasterPage | %{$_.Group[0]}
-	AddMasterPageHasCsrfTokenProperty($uniqueObjects)
-}
-
-Function AddMasterPageHasCsrfTokenProperty($objects){
-	foreach ($item in $objects){
-		if($item.MasterPage -ne 'none')
-		{
-			Write-Debug "$item"
-			$masterFilePath = $(Get-MasterPagePath($item)).FullName
-			$item | Add-Member -Name "TokenPresent" -MemberType NoteProperty -Force -Value $(Find-CsrfTokenInFile($masterFilePath)) -PassThru
-		}
-	}
-}
-
-Function Get-MasterPagesWithoutTokens([string] $path){
-	$pages = Get-AspxPages($path)
-	$uniqueMasterPages = Get-UniqueMasterPages($pages)
+Function MEMASP-GetMasterPagesWithoutTokens([string] $path){
+	$pages = MEMASP-GetAspxPages($path)
+	$uniqueMasterPages = MEMASP-GetUniqueMasterPages($pages)
 	$uniqueMasterPages | Where-Object {$_.TokenPresent -eq $false}
 }
+
+Function MEMASP-GetPagesWithNoMasterPage([string] $path){
+	$pages = MEMASP-GetAspxPages($path)
+	$pages | Where-Object {$_.MasterPage -eq $NO_MASTER_PAGE}
+}
+
