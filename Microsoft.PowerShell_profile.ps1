7912473fce7d537d6866ddb1e1fb2e4ae9265f4b
Martin Schinz <martin.schinz@memcf.co.uk>
2012-09-04 11:30:53 +0100

    added path to iis express

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,7 +5,7 @@ $env:path += ";" + $progFilesX86 + "\Git\bin"
 $env:path += ";" + $progFiles + "\Vim\vim73"
 $env:path += ";" + $prog
 $env:path += ";" + (Get-Item "Env:Windir").Value + "\Microsoft.NET\Framework64\v4.0.30319"
-
+$env:path += ";" + $progFiles + "\IIS Express"
 function Get-Batchfile ($file) {
     $cmd = "`"$file`" & set"
     cmd /c $cmd | Foreach-Object {
